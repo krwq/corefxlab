96c59d7d0257159c265338b3026549d5be81d74b
Krzysztof Wicher <kwicher@microsoft.com>
2015-10-07 18:44:19 -0700

    WIP: First implementation refactored

--- src/System.Text.Utf8/src/System/Text/Utf8/Utf8String.cs
+++ src/System.Text.Utf8/src/System/Text/Utf8/Utf8String.cs
@@ -2,209 +2,127 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Utf16;
 using System.Threading.Tasks;
 
 namespace System.Text.Utf8
 {
-    public struct Utf8String // TODO: : IEnumerable<CodePoint>
+    public struct Utf8String : IEnumerable<UnicodeCodePoint>//, IEquatable<Utf8String>, IComparable<Utf8String> // TODO:
     {
-        const uint mask_0111_1111 = 0x7F;
-        const uint mask_0011_1111 = 0x3F;
-        const uint mask_0001_1111 = 0x1F;
-        const uint mask_0000_1111 = 0x0F;
-        const uint mask_0000_0111 = 0x07;
-        const uint mask_1000_0000 = 0x80;
-        const uint mask_1100_0000 = 0xC0;
-        const uint mask_1110_0000 = 0xE0;
-        const uint mask_1111_0000 = 0xF0;
-        const uint mask_1111_1000 = 0xF8;
+        private ByteSpan _buffer;
 
-        public TextBuffer Buffer { get; set; }
+        private byte[] _bytes;
+        private int _index;
+        private int _length;
 
-        public Utf8String(TextBuffer text)
+        public Utf8String(ByteSpan buffer)
         {
-            Buffer = text;
+            _buffer = buffer;
+            _bytes = null;
+            _index = 0;
+            _length = 0;
         }
 
-        public Utf8String(byte[] buffer)
+        public Utf8String(byte[] utf8bytes) : this(utf8bytes, 0, utf8bytes.Length)
         {
-            Buffer = new TextBuffer(buffer);
         }
 
-        public static explicit operator Utf8String(string builtinString)
+        public Utf8String(byte[] utf8bytes, int index, int length)
         {
-            return new Utf8String(System.Text.Encoding.UTF8.GetBytes(builtinString));
-        }
-
-        public override string ToString()
-        {
-            return System.Text.Encoding.UTF8.GetString(Buffer.Buffer, (int)Buffer.Index, (int)Buffer.Length);
-        }
-
-        [CLSCompliant(false)]
-        public static unsafe byte* ReadCodePointByte(byte* index, ref uint codePoint)
-        {
-            codePoint <<= 6;
-            uint current = *index++;
-            if ((current & mask_1100_0000) != mask_1000_0000)
+            if (utf8bytes == null)
+            {
+                throw new ArgumentNullException("utf8bytes");
+            }
+            if (index + length > utf8bytes.Length)
+            {
                 throw new ArgumentOutOfRangeException("index");
+            }
 
-            codePoint |= mask_0011_1111 & current;
-            return index;
+            _buffer = default(ByteSpan);
+            _bytes = utf8bytes;
+            _index = index;
+            _length = length;
         }
 
-        [CLSCompliant(false)]
-        public static unsafe byte* ReadCodePointFromBuffer(byte* index, out uint codePoint)
+        #region General Implementation
+        private static IEnumerator<UnicodeCodePoint> GetCodePointsEnumerator(ByteSpan buffer)
         {
-            uint first = *index++;
-
-            if ((first & mask_1111_1000) == mask_1111_0000)
+            while (buffer.Length > 0)
             {
-                codePoint = first & mask_0000_0111;
-                index = ReadCodePointByte(index, ref codePoint);
-                index = ReadCodePointByte(index, ref codePoint);
-                index = ReadCodePointByte(index, ref codePoint);
-
-                return index;
+                UnicodeCodePoint codePoint;
+                int encodedBytes;
+                if (Utf8Encoder.TryDecodeCodePoint(buffer, out codePoint, out encodedBytes))
+                {
+                    yield return codePoint;
+                    buffer = buffer.Slice(encodedBytes);
+                }
+                else
+                {
+                    // TODO: change exception type
+                    throw new Exception("Invalid character");
+                }
             }
-
-            if ((first & mask_1111_0000) == mask_1110_0000)
-            {
-                codePoint = first & mask_0000_1111;
-                index = ReadCodePointByte(index, ref codePoint);
-                index = ReadCodePointByte(index, ref codePoint);
-
-                return index;
-            }
-
-            if ((first & mask_1110_0000) == mask_1100_0000)
-            {
-                codePoint = first & mask_0001_1111;
-                index = ReadCodePointByte(index, ref codePoint);
-
-                return index;
-            }
-
-            if ((first & mask_1000_0000) == 0)
-            {
-                codePoint = first & mask_0111_1111;
-
-                return index;
-            }
-
-            throw new Exception("InternalError");
         }
+        #endregion
 
-        private static unsafe byte* ReadNextCharacterChecked(byte* start, byte* end, out char characterOrHighSurrogate, out char lowSurrogate)
+        public IEnumerator<UnicodeCodePoint> GetEnumerator()
         {
-            byte* index = start;
-            if (index < end)
+            if (_bytes != null)
             {
-                if ((end - index) < 4)
+                unsafe
                 {
-                    int size = Utf8CodeUnits.GetNumberOfEncodedCodeUnitsFromFirstCodeUnit(*index);
-                    if (size == -1)
-                        throw new Exception("Illegal UTF8 byte.");
-                    if (index + size >= end)
-                        throw new Exception("InternalError");
-                }
-
-                uint codePoint;
-                index = ReadCodePointFromBuffer(index, out codePoint);
-
-                if (codePoint < 0x010000)
-                {
-                    characterOrHighSurrogate = (char)codePoint;
-                    lowSurrogate = (char)0;
-                    return index;
-                }
-                else 
-                {
-                    // code point needs to be represented as surrogate
-                    uint val = (codePoint - 0x010000) & 0x0FFFFF;
-
-                    characterOrHighSurrogate = (char)(0xD800 + (val >> 10));
-                    lowSurrogate = (char)(0xDC00 + (val & 0x3FF));
-                }
-            }
-
-            characterOrHighSurrogate = (char)0;
-            lowSurrogate = (char)0;
-            return null;
-        }
-
-        public unsafe long CalculateNumberOfCodePointsUnchecked()
-        {
-            long ret = 0;
-            fixed (byte* pinnedBuffer = Buffer.Buffer)
-            {
-                byte* index = pinnedBuffer + Buffer.Index;
-                byte* end = index + Buffer.Length;
-
-                while (index < end)
-                {
-                    ret++;
-                    index += Utf8CodeUnits.GetNumberOfEncodedCodeUnitsFromFirstCodeUnit(*index);
-                }
-            }
-
-            return ret;
-        }
-
-        public unsafe long CalculateNumberOfCharacters()
-        {
-            long ret = 0;
-            fixed (byte* pinnedBuffer = Buffer.Buffer)
-            {
-                byte* index = pinnedBuffer + Buffer.Index;
-                byte* end = index + Buffer.Length;
-
-                while (index < end)
-                {
-                    ret++;
-                    throw new NotImplementedException();
-                //    CodePoint cp;
-                //    index = Utf8CodeUnits.DecodeCodePoint(index, end, out cp);
-
-                //    // TODO: Utf16Encoder.DoesCodePointNeedSurrogateCharactersForItsRepresentation?
-                //    if (cp.Value >= 10000)
-                //        ret++;
-                }
-            }
-
-            return ret;
-        }
-
-        public unsafe char[] ConvertToUtf16Characters()
-        {
-            long len = CalculateNumberOfCharacters();
-            char[] ret = new char[len];
-
-            fixed (byte* pinnedBuffer = Buffer.Buffer)
-            {
-                byte* index = pinnedBuffer + Buffer.Index;
-                byte* end = index + Buffer.Length;
-
-                long idx = 0;
-                while (true)
-                {
-                    char high, low;
-                    index = ReadNextCharacterChecked(index, end, out high, out low);
-
-                    if (index == null)
-                        return ret;
-
-                    if (low != 0)
+                    fixed (byte* pinnedBytes = _bytes)
                     {
-                        ret[idx++] = high;
-                        ret[idx++] = low;
-                    }
-                    else
-                    {
-                        ret[idx++] = high;
+                        return GetCodePointsEnumerator(new ByteSpan(pinnedBytes + _index, _length));
                     }
                 }
             }
+            else
+            {
+                return GetCodePointsEnumerator(_buffer);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        //public static explicit operator Utf8String(string builtinString)
+        //{
+        //    return new Utf8String(builtinString);
+        //}
+
+        public override unsafe string ToString()
+        {
+            // get length first
+            // TODO: Optimize for characters of length 1 or 2 in UTF-8 representation (no need to read anything)
+            // TODO: is compiler gonna do the right thing here?
+            // TODO: Should we use Linq's Count()?
+            int len = 0;
+            foreach (var codePoint in this)
+            {
+                len++;
+                if (codePoint.IsSurrogate())
+                    len++;
+            }
+
+            char[] characters = new char[len];
+            fixed (char* pinnedCharacters = characters)
+            {
+                ByteSpan buffer = new ByteSpan((byte*)pinnedCharacters, len * 2);
+                foreach (var codePoint in this)
+                {
+                    int bytesEncoded;
+                    if (!Utf16LittleEndianEncoder.TryEncodeCodePoint(codePoint, buffer, out bytesEncoded))
+                    {
+                        // TODO: Change Exception type
+                        throw new Exception("invalid character");
+                    }
+                    buffer = buffer.Slice(bytesEncoded);
+                }
+            }
+            return new string(characters);
         }
     }
 }
